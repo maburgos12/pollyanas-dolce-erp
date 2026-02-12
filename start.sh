37bce6b414c691414c44c562c6ad6bed9572535d
Mauricio A Burgos <mauricioburgos@mac.lan>
2026-02-11 22:04:29 -0700

    Simplify start.sh - just run gunicorn

--- start.sh
+++ start.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 set -e
 
-echo "Running migrations..."
-python manage.py migrate
-
-echo "Collecting static files..."
-python manage.py collectstatic --noinput || true
-
 echo "Starting Gunicorn..."
-exec gunicorn config.wsgi:application --bind 0.0.0.0:$PORT --workers 1 --timeout 120
+exec gunicorn config.wsgi:application --bind 0.0.0.0:${PORT:-8000} --workers 1 --timeout 60
