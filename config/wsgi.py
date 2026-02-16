efb12769c3e47da6b023aedcad4d93fb0e108442
Mauricio A Burgos <mauricioburgos@mac.lan>
2026-02-16 12:02:41 -0700

    Add /ping endpoint for healthcheck

--- config/wsgi.py
+++ config/wsgi.py
@@ -2,4 +2,15 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
-application = get_wsgi_application()
+django_app = get_wsgi_application()
+
+def application(environ, start_response):
+    # Simple ping endpoint that doesn't require Django
+    if environ.get('PATH_INFO') == '/ping':
+        status = '200 OK'
+        response_headers = [('Content-Type', 'text/plain')]
+        start_response(status, response_headers)
+        return [b'pong']
+    
+    # Route everything else to Django
+    return django_app(environ, start_response)
