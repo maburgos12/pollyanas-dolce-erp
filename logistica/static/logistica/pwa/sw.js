c3abf5fcebf7d23264836c10c2e9102d0c65d500
Mauricio Burgos <mauricioburgos12@gmail.com>
2026-05-25 11:12:44 -0700

    fix: excepción temporal licencia pendiente Bernardo

--- logistica/static/logistica/pwa/sw.js
+++ logistica/static/logistica/pwa/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pollyanas-logistica-pwa-v4-gas-selector";
+const CACHE_NAME = "pollyanas-logistica-pwa-v5-bernardo-licencia";
 const SHELL_ASSETS = [
   "/logistica/app/",
   "/static/logistica/pwa/manifest.json",
