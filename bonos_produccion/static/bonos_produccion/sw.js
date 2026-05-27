67164720289f3c797a8ac2192f5a6a91216e99b5
Mauricio Burgos <mauricioburgos12@gmail.com>
2026-05-27 14:52:51 -0700

    Corregir reactivacion de dias en bonos ventas

--- bonos_produccion/static/bonos_produccion/sw.js
+++ bonos_produccion/static/bonos_produccion/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pollyanas-bonos-produccion-pwa-v4";
+const CACHE_NAME = "pollyanas-bonos-produccion-pwa-v5";
 const SHELL_ASSETS = [
   "/bonos-produccion/manifest.json",
   "/static/bonos_produccion/icons/icon-192.png",
