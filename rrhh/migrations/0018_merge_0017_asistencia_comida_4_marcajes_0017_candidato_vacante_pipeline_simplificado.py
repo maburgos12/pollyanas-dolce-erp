ed2c5700b013a2d5b51926e2200d6006cf46c6e6
maburgos12 <mauricioburgos12@gmail.com>
2026-06-04 17:36:35 -0700

    fix(rrhh): resolver merge de migraciones 0017
    Agrega migracion merge vacia para unir las hojas 0017 de rrhh.

---
+++ rrhh/migrations/0018_merge_0017_asistencia_comida_4_marcajes_0017_candidato_vacante_pipeline_simplificado.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("rrhh", "0017_asistencia_comida_4_marcajes"),
+        ("rrhh", "0017_candidato_vacante_pipeline_simplificado"),
+    ]
+
+    operations = []
