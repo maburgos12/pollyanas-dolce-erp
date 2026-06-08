2aa680466d92334fb14d5da2fdeee532eb99d3af
Mauricio Burgos <mauricioburgos12@gmail.com>
2026-06-07 18:03:49 -0700

    Agregar merge migration de logistica

---
+++ logistica/migrations/0016_merge_0015_repartidor_identidad_0015_reporteunidad_nullable.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("logistica", "0015_repartidor_autorizado_por_repartidor_empresa_externa_and_more"),
+        ("logistica", "0015_reporteunidad_repartidor_nullable"),
+    ]
+
+    operations = []
