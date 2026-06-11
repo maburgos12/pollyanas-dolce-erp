89fd720ff1f838c172b46c16fc99b6432fd0bfc6
maburgos12 <mauricioburgos12@gmail.com>
2026-06-11 07:27:07 -0700

    feat(conciliacion): agregar catalogo base contable fiscal (#581)

--- conciliacion/admin.py
+++ conciliacion/admin.py
@@ -1,6 +1,83 @@
 from django.contrib import admin
 
-from conciliacion.models import ImportacionBancaria
+from conciliacion.models import ConceptoConciliacion, ImportacionBancaria
+
+
+@admin.register(ConceptoConciliacion)
+class ConceptoConciliacionAdmin(admin.ModelAdmin):
+    list_display = (
+        "codigo",
+        "nombre",
+        "familia",
+        "tipo_movimiento",
+        "cfdi_esperado",
+        "forma_pago_esperada",
+        "permite_conciliacion_automatica",
+        "activo",
+    )
+    list_filter = (
+        "familia",
+        "tipo_movimiento",
+        "cfdi_esperado",
+        "afecta_iva",
+        "afecta_isr",
+        "activo",
+    )
+    search_fields = ("codigo", "nombre", "descripcion", "cuenta_contable_sugerida")
+    readonly_fields = ("creado_en", "actualizado_en")
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "codigo",
+                    "nombre",
+                    "descripcion",
+                    "familia",
+                    "tipo_movimiento",
+                    "activo",
+                    "prioridad",
+                )
+            },
+        ),
+        (
+            "Evidencia fiscal",
+            {
+                "fields": (
+                    "cfdi_esperado",
+                    "forma_pago_esperada",
+                    "requiere_rep",
+                    "requiere_cfdi_recibido",
+                    "requiere_evidencia_externa",
+                    "evidencia_requerida",
+                )
+            },
+        ),
+        (
+            "Reglas contables",
+            {
+                "fields": (
+                    "afecta_iva",
+                    "afecta_isr",
+                    "afecta_flujo",
+                    "cuenta_contable_sugerida",
+                    "palabras_clave",
+                )
+            },
+        ),
+        (
+            "Automatizacion",
+            {
+                "fields": (
+                    "permite_conciliacion_automatica",
+                    "tolerancia_monto",
+                    "tolerancia_porcentaje",
+                    "creado_en",
+                    "actualizado_en",
+                )
+            },
+        ),
+    )
 
 
 @admin.register(ImportacionBancaria)
