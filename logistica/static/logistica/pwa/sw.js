ca533ace6ec67ca529f2d26d3127359cec88c19e
Mauricio Burgos <mauricioburgos12@gmail.com>
2026-06-18 09:55:11 -0700

    chore(logistica): forzar actualizacion pwa tramo carga

--- logistica/static/logistica/pwa/sw.js
+++ logistica/static/logistica/pwa/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pollyanas-logistica-pwa-v33-api-network-only";
+const CACHE_NAME = "pollyanas-logistica-pwa-v34-tramo-carga";
 const SHELL_ASSETS = [
   "/logistica/app/",
   "/static/logistica/pwa/manifest.json",
