786afbc3440c2d5af8ee00127249fc71926c67bc
maburgos12 <mauricioburgos12@gmail.com>
2026-06-18 15:08:31 -0700

    Validar entrega PWA por producto en rutas

--- logistica/static/logistica/pwa/sw.js
+++ logistica/static/logistica/pwa/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pollyanas-logistica-pwa-v39-carga-tramo";
+const CACHE_NAME = "pollyanas-logistica-pwa-v40-entrega-productos";
 const SHELL_ASSETS = [
   "/logistica/app/",
   "/static/logistica/pwa/manifest.json",
