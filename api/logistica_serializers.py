57510f9265c1ba2749dc81415dd4c86ebb8e5949
Mauricio Burgos <mauricioburgos12@gmail.com>
2026-06-21 09:30:21 -0700

    Separar solicitado enviado y cargado en rutas

--- api/logistica_serializers.py
+++ api/logistica_serializers.py
@@ -319,8 +319,11 @@ class RutaCargaChecklistLineaSerializer(serializers.ModelSerializer):
     parada_nombre = serializers.CharField(source="parada.punto_nombre_snapshot", read_only=True)
     parada_orden = serializers.IntegerField(source="parada.orden", read_only=True)
     cantidad_solicitada = serializers.SerializerMethodField()
+    cantidad_solicitada_point = serializers.SerializerMethodField()
     cantidad_enviada_esperada = serializers.SerializerMethodField()
+    cantidad_enviada_point = serializers.SerializerMethodField()
     cantidad_cargada = serializers.SerializerMethodField()
+    cantidad_cargada_pwa = serializers.SerializerMethodField()
     estatus_display = serializers.CharField(source="get_estatus_display", read_only=True)
     motivo_diferencia_display = serializers.CharField(source="get_motivo_diferencia_display", read_only=True)
     validado_por_nombre = serializers.SerializerMethodField()
@@ -345,8 +348,11 @@ class RutaCargaChecklistLineaSerializer(serializers.ModelSerializer):
             "item_name",
             "unit",
             "cantidad_solicitada",
+            "cantidad_solicitada_point",
             "cantidad_enviada_esperada",
+            "cantidad_enviada_point",
             "cantidad_cargada",
+            "cantidad_cargada_pwa",
             "estatus",
             "estatus_display",
             "motivo_diferencia",
@@ -368,12 +374,27 @@ class RutaCargaChecklistLineaSerializer(serializers.ModelSerializer):
         return nombre_operativo_usuario(obj.validado_por)
 
     def get_cantidad_solicitada(self, obj):
+        return self.get_cantidad_solicitada_point(obj)
+
+    def get_cantidad_solicitada_point(self, obj):
+        point_line = self._point_line(obj)
+        if point_line:
+            return _format_quantity(point_line.requested_quantity)
         return _format_quantity(obj.cantidad_solicitada)
 
     def get_cantidad_enviada_esperada(self, obj):
+        return self.get_cantidad_enviada_point(obj)
+
+    def get_cantidad_enviada_point(self, obj):
+        point_line = self._point_line(obj)
+        if point_line:
+            return _format_quantity(point_line.sent_quantity)
         return _format_quantity(obj.cantidad_enviada_esperada)
 
     def get_cantidad_cargada(self, obj):
+        return self.get_cantidad_cargada_pwa(obj)
+
+    def get_cantidad_cargada_pwa(self, obj):
         return _format_quantity(obj.cantidad_cargada)
 
     def _point_line(self, obj):
@@ -405,7 +426,8 @@ class RutaCargaChecklistLineaSerializer(serializers.ModelSerializer):
         point_line = self._point_line(obj)
         if not point_line or not point_line.is_received:
             return "PENDIENTE_POINT"
-        esperado = Decimal(str(obj.cantidad_cargada if obj.cantidad_cargada is not None else obj.cantidad_enviada_esperada or 0))
+        cantidad_enviada = point_line.sent_quantity if point_line else obj.cantidad_enviada_esperada
+        esperado = Decimal(str(obj.cantidad_cargada if obj.cantidad_cargada is not None else cantidad_enviada or 0))
         recibido = Decimal(str(point_line.received_quantity or 0))
         if recibido == esperado:
             return "RECIBIDO_OK"
