2a675d0aa9cdf7b12c8b851f6059b3723beeddd7
maburgos12 <mauricioburgos12@gmail.com>
2026-06-23 18:14:24 -0700

    Robustecer PWA logistica ante capturas offline (#818)

--- logistica/static/logistica/pwa/sw.js
+++ logistica/static/logistica/pwa/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pollyanas-logistica-pwa-v54-finalizar-ruta";
+const CACHE_NAME = "pollyanas-logistica-pwa-v55-offline-acciones-finalizar-ruta";
 const SHELL_ASSETS = [
   "/logistica/app/",
   "/static/logistica/pwa/manifest.json",
