dcb715cc709182fd7348b90afb33abb4981615de
Mauricio Burgos <mauricioburgos12@gmail.com>
2026-08-29 11:32:03 -0700

    fix(mermas): estabilizar captura de fresa y producto

--- mermas/tests_form.py
+++ mermas/tests_form.py
@@ -7,7 +7,8 @@ from django.urls import reverse
 
 from core.access import ACCESS_MANAGE
 from core.models import Sucursal, UserModuleAccess, UserProfile
-from mermas.models import MermaRegistro
+from mermas.models import MermaProducto, MermaRegistro
+from recetas.models import Receta
 
 
 class MermaProductoFormularioTests(TestCase):
@@ -50,6 +51,46 @@ class MermaProductoFormularioTests(TestCase):
 
         self.assertContains(response, '"X-Requested-With": "XMLHttpRequest"')
         self.assertContains(response, "payload.error")
+
+    def test_formulario_usa_selector_nativo_de_producto_en_moviles(self):
+        receta = Receta.objects.create(
+            nombre="Pastel de Fresas con Crema Chico",
+            codigo_point="0101",
+        )
+
+        response = self.client.get(reverse("mermas:app"))
+
+        self.assertContains(response, 'select name="receta_id[]"')
+        self.assertContains(response, f'value="{receta.pk}"')
+        self.assertNotContains(response, 'list="productos-list"')
+
+    def test_post_con_selector_conserva_receta_y_nombre_como_snapshot(self):
+        receta = Receta.objects.create(
+            nombre="Pastel de Fresas con Crema Chico",
+            codigo_point="0101",
+        )
+
+        response = self.client.post(
+            reverse("mermas:app"),
+            {
+                "sucursal": self.sucursal.pk,
+                "ticket_point": "POINT-0101",
+                "producto_texto[]": [""],
+                "receta_id[]": [str(receta.pk)],
+                "cantidad[]": ["1"],
+                "ticket_fotos": [
+                    SimpleUploadedFile("ticket.jpg", b"ticket", content_type="image/jpeg"),
+                ],
+                "producto_fotos": [
+                    SimpleUploadedFile("producto.jpg", b"producto", content_type="image/jpeg"),
+                ],
+            },
+        )
+
+        producto = MermaProducto.objects.get()
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(producto.receta, receta)
+        self.assertEqual(producto.producto_texto, receta.nombre)
 
     def _branch_user(self, username, *, audits=False):
         user = get_user_model().objects.create_user(username=username, password="test")
